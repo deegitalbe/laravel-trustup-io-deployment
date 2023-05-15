ed5a900673f345d92a9bd6a2a9221dcc80f5726b
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-05-15 12:54:04 +0200

    :bug: Fixing app url generation

--- src/lib/utils/useAppUrl.ts
+++ src/lib/utils/useAppUrl.ts
@@ -1,5 +1,12 @@
+#!/usr/bin/env node
+
 const useAppUrl = (isProduction: boolean, appKey: string) => {
-  const splittedAppKey = appKey.split("-").reverse();
+  const splittedAppKey = appKey
+    .replace("trustup-io", "io-trustup")
+    .replace("worksite", "pro-worksite")
+    .split("-")
+    .reverse();
+
   if (!isProduction) splittedAppKey.unshift("staging");
   return splittedAppKey.join(".");
 };
